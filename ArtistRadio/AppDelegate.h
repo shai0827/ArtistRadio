237288b488de82396b81717915517b3c2780a626
zena_macbook <zena_macbook@zena-macbook-ui-MacBook.local>
2012-11-17 20:51:36 +0900

    Initial Commit

---
+++ ArtistRadio/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ArtistRadio
+//
+//  Created by zena_macbook on 12. 11. 17..
+//  Copyright (c) 2012년 zena_macbook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ArtistRadio/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ArtistRadio
+//
+//  Created by zena_macbook on 12. 11. 17..
+//  Copyright (c) 2012년 zena_macbook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
