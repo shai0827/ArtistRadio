ad8a617f07924105e629aefbe4a6bcdf0cf5eb3f
shai0827 <shai0827@gmail.com>
2012-11-28 17:24:12 +0900

    initail version

---
+++ ArtistRadio/RadioData.h
@@ -0,0 +1,13 @@
+//
+//  RadioData.h
+//  ArtistRadio
+//
+//  Created by zena_macbook on 12. 11. 20..
+//  Copyright (c) 2012년 zena_macbook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RadioData : NSObject
+
+@end
---
+++ ArtistRadio/RadioData.m
@@ -0,0 +1,13 @@
+//
+//  RadioData.m
+//  ArtistRadio
+//
+//  Created by zena_macbook on 12. 11. 20..
+//  Copyright (c) 2012년 zena_macbook. All rights reserved.
+//
+
+#import "RadioData.h"
+
+@implementation RadioData
+
+@end
